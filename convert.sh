49de177eb96fd6bd61529b959f740fadc6f7fe7d
juanirm <48082108+juanirm@users.noreply.github.com>
2020-03-24 10:11:52 -0300

    removed -r parameter as it was superseeded by -u

--- convert.sh
+++ convert.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-readpst -r -u -o $OUTDIR $INDIR/*.pst
+readpst -u -o $OUTDIR $INDIR/*.pst
 find out -type d | tac | grep -v '^out$' | xargs -d '\n' -I{} mv {} {}.sbd
 find out -name mbox -type f | xargs -d '\n' -I{} echo '"{}" "{}"' | sed -e 's/\.sbd\/mbox"$/"/' | xargs -L 1 mv
 find out -empty -type d | xargs -d '\n' rmdir
